d7d9dcdad7c126cf0bebf5139efb8973e2601829
Mondus <p.richmond@sheffield.ac.uk>
2022-01-26 15:52:07 +0000

    figure script completed and sample added

--- plot_publication.py
+++ plot_publication.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 import seaborn as sns
 import pandas as pd
-import numpy as np
+from matplotlib.ticker import FormatStrFormatter
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import matplotlib.image as mpimg
@@ -18,7 +18,10 @@ DEFAULT_INPUT_DIR= "./sample/data/v100-470.82.01/alpha.2-v100-11.0-beltsoff"
 DEFAULT_OUTPUT_DIR = "." #"./sample/figures/v100-470.82.01/alpha.2-v100-11.0-beltsoff"
 
 # Drift csv filename from simulation output
+SMALL_POP_BF_CSV_FILENAME = "small_pop_brute_force.csv"
 LARGE_POP_BF_CSV_FILENAME = "large_pop_brute_force.csv"
+SMALL_POP_SPATIAL_CSV_FILENAME = "small_pop.csv"
+LARGE_POP_SPATIAL_CSV_FILENAME = "large_pop.csv"
 
 
 EXPECTED_INPUT_FILES = [LARGE_POP_BF_CSV_FILENAME]
@@ -107,51 +110,98 @@ def main():
     sns.set_theme(style='white')
     
     # setup sub plot using mosaic layout
-    gs_kw = dict(width_ratios=[1, 1], height_ratios=[1, 1, 1])
-    f, ax = plt.subplot_mosaic([['p1', 'p2'],
-                                ['p3', 'p4'],
-                                ['.' , '.' ],
+    gs_kw = dict(width_ratios=[1, 1, 0.5], height_ratios=[1, 1])
+    f, ax = plt.subplot_mosaic([['p1', 'p2', '.'],
+                                ['p3', 'p4', '.'],
                                 ],
-                                  gridspec_kw=gs_kw, figsize=(7.5, 7.5),
+                                  gridspec_kw=gs_kw, figsize=(7.5, 5),
                                   constrained_layout=True)
     input_dir = pathlib.Path(args.input_dir)
     
     # common palette
-    custom_palette = {"circles_bruteforce": "g", "circles_bruteforce_rtc": "r", "circles_spatial3D": "b", "circles_spatial3D_rtc": "k"}
+    colours = sns.color_palette("viridis", 8)
+    custom_palette = {128: colours[0], 256: colours[1], 512: colours[2], 1024: colours[3], 2048: colours[4], 4096: colours[5], 6144: colours[6], 8192: colours[7]}
 
     
+    # SMALL_POP_BF_CSV_FILENAME
     # Load per simulation step data into data frame (strip any white space)
-    df = pd.read_csv(input_dir/LARGE_POP_BF_CSV_FILENAME, sep=',', quotechar='"')
+    df = pd.read_csv(input_dir/SMALL_POP_BF_CSV_FILENAME, sep=',', quotechar='"')
     df.columns = df.columns.str.strip()
-
-    serial_df = df.query('ensemble_size == 1').groupby('pop_size', as_index=False).mean()
-    speedup = []
-    for index, row in df.iterrows():
-        mean = serial_df.query(f"pop_size == {int(row['pop_size'])}")['s_sim_mean'].iloc[0]
-        speedup.append(mean / row['s_sim_mean'])
-    df["speedup"] = speedup
-
-    
-
+    # select subset of the pop sizes for plotting
+    df = df[df['pop_size'].isin([128, 256, 512, 1024])]
+    # calculate baseline and the speedup
+    df_baseline = df.query('ensemble_size == 1').groupby('pop_size', as_index=False).mean()[['pop_size',  's_sim_mean']]
+    df = df.merge(df_baseline, left_on='pop_size', right_on='pop_size', suffixes=('', '_baseline'))
+    df["speedup"] = df['s_sim_mean_baseline'] / df['s_sim_mean']
     # Plot popsize brute force
-    plt_df_bf = sns.lineplot(x='ensemble_size', y='speedup', hue='pop_size', style='pop_size', data=df, ax=ax['p1'], ci="sd")
-    # plt_df_bf.ticklabel_format(style='plain', axis='x') # no scientific notation
-    #plt_df_bf.set(xlabel='N', ylabel='Step time (s)')
-    #ax['p1'].set_title(label='a', loc='left', fontweight="bold")
-    #ax['p1'].legend().set_visible(False)
-
+    plt_df_bf = sns.lineplot(x='ensemble_size', y='speedup', hue='pop_size', style='pop_size', data=df, palette=custom_palette, ax=ax['p1'], ci="sd")
+    plt_df_bf.set(xlabel='', ylabel='Speedup')
+    # set tick formatting, title and hide legend
+    ax['p1'].yaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+    ax['p1'].set_title(label='A', loc='left', fontweight="bold")
+    ax['p1'].legend().set_visible(False)
+    
+    # SMALL_POP_SPATIAL_CSV_FILENAME
+    # Load per simulation step data into data frame (strip any white space)
+    df = pd.read_csv(input_dir/SMALL_POP_SPATIAL_CSV_FILENAME, sep=',', quotechar='"')
+    df.columns = df.columns.str.strip()
+    # select subset of the pop sizes for plotting
+    df = df[df['pop_size'].isin([128, 256, 512, 1024])]
+    # calculate baseline and the speedup
+    df_baseline = df.query('ensemble_size == 1').groupby('pop_size', as_index=False).mean()[['pop_size',  's_sim_mean']]
+    df = df.merge(df_baseline, left_on='pop_size', right_on='pop_size', suffixes=('', '_baseline'))
+    df["speedup"] = df['s_sim_mean_baseline'] / df['s_sim_mean']
+    # Plot popsize brute force
+    plt_df_bf = sns.lineplot(x='ensemble_size', y='speedup', hue='pop_size', style='pop_size', data=df, palette=custom_palette, ax=ax['p2'], ci="sd")
+    plt_df_bf.set(xlabel='', ylabel='')
+    # set tick formatting, title and hide legend
+    ax['p2'].yaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+    ax['p2'].set_title(label='B', loc='left', fontweight="bold")
+    ax['p2'].legend().set_visible(False)
+    
+    # LARGE_POP_SPATIAL_CSV_FILENAME
+    # Load per simulation step data into data frame (strip any white space)
+    df = pd.read_csv(input_dir/LARGE_POP_SPATIAL_CSV_FILENAME, sep=',', quotechar='"')
+    df.columns = df.columns.str.strip()
+    # select subset of the pop sizes for plotting
+    df = df[df['pop_size'].isin([2048, 4096, 6144, 8192])]
+    # calculate baseline and the speedup
+    df_baseline = df.query('ensemble_size == 1').groupby('pop_size', as_index=False).mean()[['pop_size',  's_sim_mean']]
+    df = df.merge(df_baseline, left_on='pop_size', right_on='pop_size', suffixes=('', '_baseline'))
+    df["speedup"] = df['s_sim_mean_baseline'] / df['s_sim_mean']
+    # Plot popsize brute force
+    plt_df_bf = sns.lineplot(x='ensemble_size', y='speedup', hue='pop_size', style='pop_size', data=df, palette=custom_palette, ax=ax['p3'], ci="sd")
+    plt_df_bf.set(xlabel='E', ylabel='Speedup')
+    # set tick formatting, title and hide legend
+    ax['p3'].yaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+    ax['p3'].set_title(label='C', loc='left', fontweight="bold")
+    ax['p3'].legend().set_visible(False)
     
     
-
-   
+    # LARGE_POP_SPATIAL_CSV_FILENAME
+    # Load per simulation step data into data frame (strip any white space)
+    df = pd.read_csv(input_dir/LARGE_POP_SPATIAL_CSV_FILENAME, sep=',', quotechar='"')
+    df.columns = df.columns.str.strip()
+    # select subset of the pop sizes for plotting
+    df = df[df['pop_size'].isin([2048, 4096, 6144, 8192])]
+    # calculate baseline and the speedup
+    df_baseline = df.query('ensemble_size == 1').groupby('pop_size', as_index=False).mean()[['pop_size',  's_sim_mean']]
+    df = df.merge(df_baseline, left_on='pop_size', right_on='pop_size', suffixes=('', '_baseline'))
+    df["speedup"] = df['s_sim_mean_baseline'] / df['s_sim_mean']
+    # Plot popsize brute force
+    plt_df_bf = sns.lineplot(x='ensemble_size', y='speedup', hue='pop_size', style='pop_size', data=df, palette=custom_palette, ax=ax['p4'], ci="sd")
+    plt_df_bf.set(xlabel='E', ylabel='')
+    # set tick formatting, title and hide legend
+    ax['p4'].yaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+    ax['p4'].set_title(label='D', loc='left', fontweight="bold")
+    ax['p4'].legend().set_visible(False)
     
-
-    # Legend
-    #lines_labels = [ax.get_legend_handles_labels() for ax in f.axes]
-    #lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
-    #labels = [MODEL_NAME_MAP[l] for l in labels] # Rename labels to provide readable legends
-    #unique = {k:v for k, v in zip(labels, lines)} 
-    #f.legend(unique.values(), unique.keys(), loc='lower center')
+    
+    # Figure Legend from unique lines in pallet
+    lines_labels = [ax.get_legend_handles_labels() for ax in f.axes]
+    lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
+    unique = {k:v for k, v in zip(labels, lines)} 
+    f.legend(unique.values(), unique.keys(), loc='upper right', title='N')
 
     
         
